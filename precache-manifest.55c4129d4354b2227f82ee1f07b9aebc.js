d8fd9252491a2e35edbe3d142d68d9573acc5da1
Andrei <AndreiM150@mail.ru>
2020-01-28 19:48:34 +0300

    Updates

---
+++ precache-manifest.55c4129d4354b2227f82ee1f07b9aebc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b82db1e074d713009bf95be86ba71dfa",
+    "url": "/AKTestingProject/index.html"
+  },
+  {
+    "revision": "8f67721fd40441c6b1e4",
+    "url": "/AKTestingProject/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "fcb09cc1a605969d4477",
+    "url": "/AKTestingProject/static/css/main.3601e45f.chunk.css"
+  },
+  {
+    "revision": "8f67721fd40441c6b1e4",
+    "url": "/AKTestingProject/static/js/2.bf80dd39.chunk.js"
+  },
+  {
+    "revision": "6aeb469c0c20fd3334c86de961c4692c",
+    "url": "/AKTestingProject/static/js/2.bf80dd39.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fcb09cc1a605969d4477",
+    "url": "/AKTestingProject/static/js/main.0e8231f3.chunk.js"
+  },
+  {
+    "revision": "d7fcc7d1a74154608012",
+    "url": "/AKTestingProject/static/js/runtime-main.559be4cb.js"
+  }
+]);
